e0c768966c5be8c4ae4ba8b6d976ef7216e7ce1f
kartikmishraa <kartikm2219@gmail.com>
2023-05-16 11:36:30 +0530

    first commit

---
+++ server.js
@@ -0,0 +1,16 @@
+const express = require("express");
+const dotenv = require("dotenv").config();
+
+const app = express();
+const PORT = process.env.PORT || 5000;
+
+// Body Parser
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+
+// Route
+app.use("/api/user", require("./Routes/userRoute"));
+
+app.listen(PORT, () => {
+  console.log(`server running at port: ${PORT}`);
+});
